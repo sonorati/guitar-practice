8d76c9113c0d4012fc6f8ba215908dad41949bab
seon <sebastian@seonsoftwarefactory.com>
2019-12-30 21:22:00 +0000

    remove accidentals

--- src/main/kotlin/com/seon/guitarpractice/Note.kt
+++ src/main/kotlin/com/seon/guitarpractice/Note.kt
@@ -1,27 +1,25 @@
 package com.seon.guitarpractice
 
-import com.seon.guitarpractice.Accidental.*
-
-enum class Note(val tabName: String, val accidentals: Accidental) {
-    A("A", Perfect),
-    ASharp("A#", Sharp),
-    BFlat("B♭", Flat),
-    B("B", Perfect),
-    BSharp("B#", Sharp),
-    CFlat("C♭", Flat),
-    C("C", Perfect),
-    CSharp("C#", Sharp),
-    DFlat("D♭", Flat),
-    D("D", Perfect),
-    DSharp("D#", Sharp),
-    EFlat("E♭", Flat),
-    E("E", Perfect),
-    ESharp("E#", Sharp),
-    FFlat("F♭", Flat),
-    F("F", Perfect),
-    FSharp("F#", Sharp),
-    GFlat("G♭", Flat),
-    G("G", Perfect),
-    GSharp("G#", Sharp),
-    AFlat("A♭", Flat)
+enum class Note(val tabName: String) {
+    A("A"),
+    ASharp("A#"),
+    BFlat("B♭"),
+    B("B"),
+    BSharp("B#"),
+    CFlat("C♭"),
+    C("C"),
+    CSharp("C#"),
+    DFlat("D♭"),
+    D("D"),
+    DSharp("D#"),
+    EFlat("E♭"),
+    E("E"),
+    ESharp("E#"),
+    FFlat("F♭"),
+    F("F"),
+    FSharp("F#"),
+    GFlat("G♭"),
+    G("G"),
+    GSharp("G#"),
+    AFlat("A♭")
 }
