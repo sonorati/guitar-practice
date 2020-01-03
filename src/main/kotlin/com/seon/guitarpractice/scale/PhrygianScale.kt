c8a92c2350f653e9c39d1429e5d62ff1923dcef5
Sebastian.Onorati <sebastian.onorati@10xbanking.com>
2020-01-03 11:59:16 +0000

    fixed phrygian notes

--- src/main/kotlin/com/seon/guitarpractice/scale/PhrygianScale.kt
+++ src/main/kotlin/com/seon/guitarpractice/scale/PhrygianScale.kt
@@ -5,9 +5,14 @@ import com.seon.guitarpractice.Note
 import com.seon.guitarpractice.Type
 import com.seon.guitarpractice.Type.*
 
+/**
+ *   Basic Observations
+ *   First notice that it has a b3 (minor 3rd) so it's a minor type scale.
+ *   It also has a b7 which makes it work on Min7 type chords. It's the b2 and b6 (which can be thought of as a #5) which are both very strong
+ *   Key Tone = b2
+ *   The b2 is the main flavour here, that interval is only found in one other mode (Locrian).
+ */
 data class PhrygianScale(val key: Note, val type: Type = Minor) : Scale(key, type) {
-
-    //TODO: FIXME CHORDS FROM MINOR SCALE!!
 
     override fun scaleNotes(): List<Note> = when (key) {
         in listOf(Note.ESharp, Note.CSharp, Note.BSharp) -> ScaleTemplate.allNotesWithSharpsAlt
@@ -16,12 +21,12 @@ data class PhrygianScale(val key: Note, val type: Type = Minor) : Scale(key, typ
     }
 
     override val interval = listOf(
-            Interval(ROOT, Major),
+            Interval(ROOT, Minor),
             Interval(FLAT_SECOND, Major),
-            Interval(MINOR_THIRD, Minor),
-            Interval(FOURTH, Diminished),
-            Interval(FIFTH, Major),
-            Interval(FLAT_SIXTH, Minor),
+            Interval(MINOR_THIRD, Major),
+            Interval(FOURTH, Minor),
+            Interval(FIFTH, Diminished),
+            Interval(FLAT_SIXTH, Major),
             Interval(FLAT_SEVENTH, Minor)
     )
 }
--- src/test/kotlin/com/seon/guitarpractice/TempMain.kt
+++ src/test/kotlin/com/seon/guitarpractice/TempMain.kt
@@ -4,81 +4,123 @@ import com.seon.guitarpractice.Note.*
 import com.seon.guitarpractice.Progression.*
 import com.seon.guitarpractice.Type.Major
 import com.seon.guitarpractice.Type.Minor
-import com.seon.guitarpractice.scale.MixolydianScale
-import com.seon.guitarpractice.scale.ScaleService
+import com.seon.guitarpractice.scale.*
 
 fun main() {
 
-    repeat((1..10).count()) {
-        println("${it + 1}. ${randomTask()}")
-    }
+//    repeat((1..10).count()) {
+//        println("${it + 1}. ${generateTask()}")
+//    }
+    processScales(C)
 }
 
+val triads = listOf(Major, Major, Minor).random()
+val threeNotesPerStringPosition = listOf(1, 2, 3, 4, 5, 6, 7).random()
+val scales = listOf("major", "major", "minor pentatonic").random()
+val notes = listOf(A, ASharp, B, C, CSharp, D, DSharp, E, F, FSharp, G, GSharp).random()
+val seventh = listOf("7", "maj7", "m7", "m7b5").random()
+val triadsStrings = listOf("4", "3").random()
+val string = listOf("6", "5", "4", "3").random()
+val mixolydianScale = MixolydianScale(notes)
 val scaleService = ScaleService()
+val scalePositions = listOf("1", "2", "3", "4", "5", "all").random()
 
-fun randomTask(): String {
+val minorScaleChordProgressions = listOf(
+        listOf(i, VI, III, VII),
+        listOf(i, VI, VII, v),
+        listOf(i, VII, VI, v)
+).random()
 
-    val triads = listOf(Major, Major, Minor).random()
-    val threeNotesPerStringPosition = listOf(1, 2, 3, 4, 5, 6, 7).random()
-    val scales = listOf("major", "major", "minor pentatonic").random()
-    val notes = listOf(A, ASharp, B, C, CSharp, D, DSharp, E, F, FSharp, G, GSharp).random()
-    val seventh = listOf("7", "maj7", "m7", "m7b5").random()
-    val triadsStrings = listOf("4", "3").random()
-    val string = listOf("6", "5", "4", "3").random()
-    val mixolydianScale = MixolydianScale(notes)
-    val minorScaleChordProgressions = listOf(
-            listOf(i, VI, III, VII),
-            listOf(i, VI, VII, v),
-            listOf(i, VII, VI, v)
-    ).random()
-    val majorScaleChordProgressions = listOf(
-            listOf(I, vi, IV, V),
-            listOf(I, V, vi, iii, IV, iii, IV, V),
-            listOf(I, iii, vi, IV),
-            listOf(I, iii, I, iii),
-            listOf(I, V, iii, VI),
-            listOf(I, V, vi, IV),
-            listOf(I, ii, iii, ii),
-            listOf(I, IV, vi, V),
-            listOf(IV, V, I)
-    ).random()
-    val scalePositions = listOf("1", "2", "3", "4", "5", "all").random()
-    val exercises = listOf(
-//            "${notes.name} mixolydian chords: ${scaleChords(mixolydianScale)}",
-            "$triads scale in ${notes.tabName}, position: $scalePositions",
-            "Major scale 3 notes per string position: $threeNotesPerStringPosition",
-            "Minor pentatonic scale in ${notes.tabName}, position: $scalePositions",
-//            "$triads arpeggios note: $notes, position: $scalePositions",
-            "Intervals, name distance and note from $string string, Note ${notes.tabName}",
-            "Chord construction: ${notes.tabName}$seventh from $string string",
-            "Minor Plagal Cadence, Note ${notes.tabName}",
-            "Plagal Cadence, Note ${notes.tabName}",
-            "Perfect Cadence, Note: ${notes.tabName}"
-    )
+val majorScaleChordProgressions = listOf(
+        listOf(I, vi, IV, V),
+        listOf(I, V, vi, iii, IV, iii, IV, V),
+        listOf(I, iii, vi, IV),
+        listOf(I, iii, I, iii),
+        listOf(I, V, iii, VI),
+        listOf(I, V, vi, IV),
+        listOf(I, ii, iii, ii),
+        listOf(I, IV, vi, V),
+        listOf(IV, V, I)
+).random()
 
-    val triadExercises = listOf(
-            "Find notes in the fretboard: ${notes.tabName}",
-            "$triads triads from ${triadsStrings}th string, Note: ${notes.tabName} find 3rd and 5th note",
-//            "riff and lead Harmonic Minor (https://www.youtube.com/watch?v=2kSq9GtZu_k)",
+fun generateTask(): String {
+
+    //1. arpeggios
+    //2. riffs & lead
+    //3. chord progression
+    //4. technique
+    //5. find notes and chords
+    //6. scales
+
+    val exercises = listOf("")
+
+    return exercises.random()
+}
+
+val arpeggiosExercises = listOf(
+        "$triads arpeggios note: $notes, position: $scalePositions"
+)
+
+val riffAndLeadExercises = listOf(
+        "riff and lead Harmonic Minor (https://www.youtube.com/watch?v=2kSq9GtZu_k)",
+        "Metallica riff (https://www.youtube.com/watch?v=UuqvZDDm_bk)"
+)
+
+val progressionExercises = listOf(
 //            "Progression with borrowed chords (https://www.youtube.com/watch?v=7IdttvJSedg)",
 //            "Progression with 7 (https://www.youtube.com/watch?v=3JizNRwHYNY)",
 //            "Progression with secondary dominants (https://www.youtube.com/watch?v=py4HaueW50Q)",
 //            "Progression with Motifs and Chords (https://www.youtube.com/watch?v=z3Dy6Mnp5Og&t=332s)",
-            """$majorScaleChordProgressions progression from string: $triadsStrings
+        "Minor Plagal Cadence, Note ${notes.tabName}",
+        "Plagal Cadence, Note ${notes.tabName}",
+        "Perfect Cadence, Note: ${notes.tabName}",
+        """$majorScaleChordProgressions progression from string: $triadsStrings
    ${getIntervals(notes, majorScaleChordProgressions)}""".trim(),
-            """ $minorScaleChordProgressions  progression from string:  $triadsStrings
+        """ $minorScaleChordProgressions  progression from string:  $triadsStrings
     ${getIntervals(notes, minorScaleChordProgressions)}""".trimIndent()
-    )
+)
 
-    val riffExercises = listOf("Metallica riff")
+val techniqueExercises = listOf(
+        "legato",
+        "sweeping arpeggios"
+)
 
-    return triadExercises.random()
-}
+val findNotesAndChordExercises = listOf(
+        "Intervals, name distance and note from $string string, Note ${notes.tabName}",
+        "Chord construction: ${notes.tabName}$seventh from $string string",
+        "Find notes in the fretboard: ${notes.tabName}",
+        "$triads triads from ${triadsStrings}th string, Note: ${notes.tabName} find 3rd and 5th note"
+)
 
-private fun scaleChords(mixolydianScale: MixolydianScale): String {
-    return mixolydianScale.chords().fold("|") { acc, chord -> "$acc ${chord.name()} | " }
-}
+val scaleExercises = listOf(
+        "${notes.name} mixolydian chords: ${scaleChords(mixolydianScale)}",
+        "$triads scale in ${notes.tabName}, position: $scalePositions",
+        "Major scale 3 notes per string position: $threeNotesPerStringPosition",
+        "Minor pentatonic scale in ${notes.tabName}, position: $scalePositions"
+)
 
 fun getIntervals(key: Note, progressions: List<Progression>): String {
     return scaleService.getChordInterval(key, progressions).map { it.name() }.toString()
+}
+
+fun processScales(key: Note) {
+    allModes(key).forEach { scale ->
+        println("${scale.javaClass.simpleName}    Chords: ${scaleChords(scale)}")
+    }
+}
+
+fun allModes(key: Note): List<Scale> {
+    return listOf(
+            MajorScale(key),
+            DorianScale(key),
+            PhrygianScale(key),
+            LydianScale(key),
+            MixolydianScale(key),
+            MinorScale(key),
+            LocrianScale(key)
+    )
+}
+
+fun scaleChords(mixolydianScale: Scale): String {
+    return mixolydianScale.chords().fold("|") { acc, chord -> "$acc ${chord.name()} | " }
 }
